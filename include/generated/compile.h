492995b2a710569e791d94975972a43a578e304c
steve.cha <aireat@gmail.com>
2019-11-23 00:55:43 +0900

    [추가] 커널 빌드시 생성된 헤더파일 추가

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Nov 22 23:00:11 KST 2019"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "aireat-VirtualBox"
+#define LINUX_COMPILER "gcc version 7.4.0 (Ubuntu/Linaro 7.4.0-1ubuntu1~18.04.1)"
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "5.1.0-00003-g76f5bd0fbde1-dirty"
